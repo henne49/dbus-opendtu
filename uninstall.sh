f75bef813de1f43fefd15f7408016ff3ba691d66
henne49 <christian@ehennrich.com>
2024-06-06 15:42:28 +0200

    fix uninstall-sh script not removing service

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
-filename=/data/rc.local
+RC_LOCAL_FILE=/data/rc.local
 
 #remove the service
 rm /service/$SERVICE_NAME
 
 # end the dbus-opendtu process
-kill $(pgrep -f 'supervise dbus-opendtu')
+kill $(pgrep -f 'dbus-opendtu')
 
 # delete old logs if they exist  
 if [ -f $SCRIPT_DIR/current.log ]; then  
     rm $SCRIPT_DIR/current.log*  
 fi 
+
+# remove install.sh from rc.local
+STARTUP=$SCRIPT_DIR/install.sh
+sed -i "\~$STARTUP~d" $RC_LOCAL_FILE
