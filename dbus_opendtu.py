1e1190b628483d1059b4ba815bbaba16dfa091a6
0x7878 <17197791+0x7878@users.noreply.github.com>
2024-11-12 13:20:42 +0000

    Refactor imports in dbus_opendtu.py

--- dbus_opendtu.py
+++ dbus_opendtu.py
@@ -1,28 +1,7 @@
 #!/usr/bin/env python
 '''module to read data from dtu/template and show in VenusOS'''
 
-# File specific rules
-# pylint: disable=broad-except
-
-# system imports:
-import logging
-import logging.handlers
-import os
-import configparser
-import sys
-
-# our imports:
-import constants
-import tests
-from helpers import *
-
-# Victron imports:
-from dbus_service import DbusService
-
-if sys.version_info.major == 2:
-    import gobject  # pylint: disable=E0401
-else:
-    from gi.repository import GLib as gobject  # pylint: disable=E0401
+from imports import *
 
 config = None
 paths = None
