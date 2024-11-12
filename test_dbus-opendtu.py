964265732a0ea21324fdbeec7928473b0908cba8
0x7878 <17197791+0x7878@users.noreply.github.com>
2024-11-12 13:20:55 +0000

    Refactor test_dbus-opendtu.py to remove unused mock_constants parameter in test_register_service method

--- test_dbus-opendtu.py
+++ test_dbus-opendtu.py
@@ -1,7 +1,8 @@
+""" Unit tests for the dbus_opendtu.py module """
+
 import unittest
 from unittest.mock import patch, MagicMock
 import sys
-import os
 
 # Mocking the dbus and other dependencies before importing the module to test
 sys.modules['dbus'] = MagicMock()
@@ -9,18 +10,17 @@ sys.modules['vedbus'] = MagicMock()
 sys.modules['gi.repository'] = MagicMock()
 sys.modules['dbus.mainloop.glib'] = MagicMock()
 
-# Add the directory containing dbus_opendtu.py to the Python path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from dbus_opendtu import register_service  # pylint: disable=E0401,C0413 # noqa: E402
 
-from dbus_opendtu import register_service
 
 class TestRegisterService(unittest.TestCase):
+    """ Test cases for the register_service function """
+
     @patch('dbus_opendtu.config', {'DEFAULT': {'DTU': 'opendtu'}})
     @patch('dbus_opendtu.DbusService')
     @patch('dbus_opendtu.get_config_value')
-    @patch('dbus_opendtu.constants')
-    
     def test_register_service(self, mock_get_config_value, mock_dbus_service):
+        """ Test the register_service function """
         def get_config_value_side_effect(key, *args, **kwargs):
             if isinstance(key, dict):
                 key = key.get('key', 'mock_value')
@@ -31,7 +31,6 @@ class TestRegisterService(unittest.TestCase):
             }.get(key, 'mock_value')
 
         mock_get_config_value.side_effect = get_config_value_side_effect
-        #mock_dbus_service.return_value = MagicMock()
         mock_dbus_service_instance = mock_dbus_service.return_value
         mock_dbus_service_instance.get_number_of_inverters.return_value = 1
 
@@ -43,9 +42,9 @@ class TestRegisterService(unittest.TestCase):
         # Additional assertions
         mock_dbus_service.assert_called_once_with(
             servicename="mock_value",
-            paths=unittest.mock.ANY,
             actual_inverter=0,
         )
+
 
 if __name__ == '__main__':
     unittest.main()
